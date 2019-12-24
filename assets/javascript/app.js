b44c345e49234455a44653863adbe81c20812abd
xiongkb <mkan-bx@hotmail.com>
2019-12-24 15:38:54 -0800

    initial files and html set up

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// the show screen where start needs to be pressed
+function start() {
+    $(".start").on("click", function(){
+        $(this).hide();
+    });
+}
+// start of the game
+function beginGame() {
+    
+}
