fb4dd2992b585e23e2ac1ef38dff4533da0fb077
xiongkb <mkan-bx@hotmail.com>
2019-12-27 16:23:09 -0800

    fixed question and added results screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,7 +13,7 @@ start();
 
 function clock(){
     $(this).hide();
-    startTime = 5;
+    startTime = 15;
     timer();
     seconds = setInterval(timer, 1000);
     quiz();
@@ -21,12 +21,40 @@ function clock(){
 
 // timer start
 function timer() {
+    $(".body").show();
     $(".timer").text(startTime);
     startTime = startTime - 1;
     if (startTime < 0) {
         clearInterval(seconds);
         counter ++;
+        missed ++;
         clock();
+    }
+}
+
+$(".a").on("click", function (){clicker("a")});
+$(".b").on("click", function (){clicker("b")});
+$(".c").on("click", function (){clicker("c")});
+$(".d").on("click", function (){clicker("d")});
+
+function clicker(picked){
+    if (picked === theQuestions[i].answer) {
+        $(".body").hide();
+        $(".screen").html("<img alt='cat is right' src='./assets/images/rightcat.jpeg'>");
+        right ++;
+        counter ++;
+        clearInterval(seconds)
+        setTimeout(quiz, 5000);
+        setTimeout(timer, 5000);
+        setTimeout(clock, 5000);
+    } else {
+        $(".body").hide();
+        $(".screen").html("<p>NO!! You are wrong!</p><img alt='cat meme' src='./assets/images/womenyellingcat.jpg'><p>The answer is " + theQuestions[i].answer);
+        wrong ++;
+        counter ++;
+        clearInterval(seconds);
+        setTimeout(quiz, 5000);
+        setTimeout(clock, 5000);
     }
 }
 
@@ -35,60 +63,60 @@ var theQuestions = [
     {
         question: "What is a tabbie?",
         choices: {
-            a: "A cat breed",
-            b: "A cat coat pattern",
-            c: "A cat color",
-            d: "A cat personality"
+            a: "a. A cat breed",
+            b: "b. A cat coat pattern",
+            c: "c. A cat color",
+            d: "d. A cat personality"
         },
         answer: "b"
     },
     {
         question: "A cat wagging their tail means: ",
         choices: {
-            a: "They are happy",
-            b: "They are getting upset",
-            c: "They are shooing away flies",
-            d: "They want to play"
+            a: "a. They are happy",
+            b: "b. They are getting upset",
+            c: "c. They are shooing away flies",
+            d: "d. They want to play"
         },
         answer: "b"
     },
     {
         question: "A female cat is also called: ",
         choices: {
-            a: "A Queen",
-            b: "An Alpha",
-            c: "A Ewe",
-            d: "A Heifer"
+            a: "a. A Queen",
+            b: "b. An Alpha",
+            c: "c. A Ewe",
+            d: "d. A Heifer"
         },
         answer: "a"
     },
     {
         question: "A cat can rotate their ears to: ",
         choices: {
-            a: "60 degrees",
-            b: "100 degrees",
-            c: "140 degrees",
-            d: "180 degrees"
+            a: "a. 60 degrees",
+            b: "b. 100 degrees",
+            c: "c. 140 degrees",
+            d: "d. 180 degrees"
         },
         answer: "d"
     },
     {
         question: "A cat has how many eyelids?",
         choices: {
-            a: "1",
-            b: "2",
-            c: "3",
-            d: "4"
+            a: "a. 1",
+            b: "b. 2",
+            c: "c. 3",
+            d: "d. 4"
         },
         answer: "c"
     },
     {
         question: "Cats with extra toes are called: ",
         choices: {
-            a: "Teratactyle Cats",
-            b: "Polydactyle Cats",
-            c: "Manidactyle Cats",
-            d: "Binarytactyle Cats"
+            a: "a. Teratactyle Cats",
+            b: "b. Polydactyle Cats",
+            c: "c. Manidactyle Cats",
+            d: "d. Binarytactyle Cats"
         },
         answer: "b"
     }
@@ -96,10 +124,21 @@ var theQuestions = [
 
 // showing questions to html
 function quiz() {
+    $(".screen").html("");
     i = counter; //picking the question
-    $(".question").text(theQuestions[i].question);
+    
+    console.log(counter)
+    if (counter > 5) {
+        console.log("test")
+        $(".game-contents").hide();
+        $(".result").html("<p>RESULTS</p><p>Answered Right: "+right+"<p>Answered Wrong:"+wrong+"<p>Missed: "+missed);
+        $(".reset").html("Click here to RESTART")
+    } else {
+        $(".question").text(theQuestions[i].question);
     $(".a").text(theQuestions[i].choices.a);
     $(".b").text(theQuestions[i].choices.b);
     $(".c").text(theQuestions[i].choices.c);
     $(".d").text(theQuestions[i].choices.d);
+    }
 }
+
